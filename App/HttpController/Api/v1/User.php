82c3d3a88c0f7935241f0123d13c56e214b4ece6
李沈阳 <lsy_php@163.com>
2019-07-23 09:59:38 +0800

    aaa

---
+++ App/HttpController/Api/v1/User.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lishenyang
+ * Date: 2019-07-23
+ * Time: 09:56
+ */
+
+namespace App\HttpController\Api\v1;
+
+
+use App\HttpController\Base;
+
+class User extends Base
+{
+
+    public function getUserData() {
+
+    }
+}
